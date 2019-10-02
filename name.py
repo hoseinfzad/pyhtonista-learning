a9ada242bcac190c7e114be96e86838b55fe999e
Prince Adrian <princewurl510@gmail.com>
2019-10-01 18:56:38 -0700

    Working and manipulating string in python

---
+++ name.py
@@ -0,0 +1,24 @@
+name = "ada lovelace"
+
+# Make the first letter of each word captialize
+print(name.title())
+
+print(name.upper())
+print(name.lower())
+
+first_name = "ada"
+last_name = "lovelace"
+full_name = first_name + " " + last_name
+
+print("Hello, " + full_name.title() + "!")
+
+message = "Hello, " + full_name.title() + "!"
+print(message)
+
+# To get rid of white spaces in strings from the right hand side
+right_favorite_lanuage = 'python '
+right_favorite_lanuage.rstrip()
+
+# To get rid of white spaces in strings for the left hand side
+left_favorite_lanuage = ' python'
+left_favorite_lanuage.lstrip()
