8869dc47bfd02350ce9f34b16f93c5bea2670220
Prince Adrian <princewurl510@gmail.com>
2019-10-02 19:44:47 -0700

    String exersises chaper two

--- favorite_number.py
+++ favorite_number.py
@@ -1,3 +1,5 @@
+# displaying the programmer favorite number
+# Adrian Anderson 10/02/2019
 favorite_number = 12
 message = "My favorite number is " + str(favorite_number)
 print(message)
--- name_cases.py
+++ name_cases.py
@@ -1,3 +1,5 @@
+# Using the various string methods to print the upper, lower and title cases of a string
+# Adrian Anderson 10/02/2019
 name = "adriaN aNderson"
 print(name)
 print(name.upper())
